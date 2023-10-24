2139ba2571faf5be1e5dd8f6140436d7309a3076
clara7227 <clarasalgado27@gmail.com>
2023-10-24 16:05:50 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,63 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Contador from "./Contador"
+import React, { useState, createContext } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Contador from "./Contador";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // CTRL K + U Uncomment en bloque
-root.render(
- <React.StrictMode>
-   <App />
- </React.StrictMode>
-);
+// root.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>
+// );
+const UserContext = createContext();
+function Sidebar() {
+  // Este usuario normalmente lo obtendriamos desde alguna una API. Para
+  // efectos de este ejemplo nos saltamos ese paso y guardamos directamene
+  // el valor.
 
-
-
+  return (
+    <UserContext.Consumer>
+      {(user) => (
+        <div class="Sidebar">
+          <img alt="Profile" src={user.avatarUrl} />
+          <h2>{user.name}</h2>
+          <h1>{user.username}</h1>
+          <div>
+            Seguidores: {user.followers}, Seguidos: {user.following}, Estrellas:{" "}
+            {user.stars}
+          </div>
+        </div>
+      )}
+    </UserContext.Consumer>
+  );
+}
+function Content() {
+  return (
+    <div class="Content">
+      <h2>Proyectos</h2>
+    </div>
+  );
+}
+function Header() {
+  return ( <UserContext.Consumer>
+    {(user) => (
+    <div class="Header">Sesión iniciada como @{user.username}
+    </div>
+    )}
+    </UserContext.Consumer>
+    );
+}
 //CTRL + K +C Comment en bloque
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {date:new Date()};
-//   }
-//   componentDidMount() { //creacion del componente
-//     this.timerID = setInterval(
-//       () => this.tick(), //método - funcion dentro de un objeto. El objeto es el que se crea a partir del constructor dentro de la funcion class
-//       10000)
-//   }
+export default function App() {
+  const [user] = useState({
+    username: "muZk",
+    name: "Nicolás Gómez",
+    bio: "Full Stack Developer",
+    avatarUrl: "https://codewithnico.com/assets/images/about.png",
+    following: 100,
+    followers: 100,
+    stars: 10,
+  });
+  return (
+    <div className="App">
+      <UserContext.Provider value={user}>
+        <Header />
+        <Sidebar />
+        <Content />
+      </UserContext.Provider>
+    </div>
+  );
+}
 
- 
-//   componentWillUnmount() {
-//     clearInterval(this.timerID);
-//   }
-
-//   tick() {
-//     this.setState({ //cada vez que cambia setState, se invoca el método render
-//       date: new Date()
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Contador/>
-//         <h1>Hello, world!</h1>
-//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//   }
-// }
-
- 
-//   root.render(<Clock/>, <Contador/>);
-
-
-
+root.render(<App />, <Contador />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 reportWebVitals();
-
